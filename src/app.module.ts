e0a439a4319b545bfa5433e2671ee7255fbb1ac4
devjohn42 <devjohncb@gmail.com>
2026-08-24 22:22:16 -0300

    :gear: config: docker and env variables config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { databaseConfig } from './config/database.config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		TypeOrmModule.forRoot(databaseConfig)
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+
+	app.enableCors()
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true
+		})
+	)
+
+	const port = process.env.PORT ?? 3004
+	await app.listen(port)
+
+	console.log(`💳 Payments Service running on port ${port}`)
 }
-bootstrap();
+bootstrap()
