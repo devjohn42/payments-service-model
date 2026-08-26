ed4c55884e9e122c4be8d594535fd82e5afa603a
devjohn42 <devjohncb@gmail.com>
2026-08-25 21:11:58 -0300

    :sparkles: feat: creates the initial structure for consuming messages in the payment-service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { databaseConfig } from './config/database.config'
+import { EventsModule } from './events/events.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true
 		}),
-		TypeOrmModule.forRoot(databaseConfig)
+		TypeOrmModule.forRoot(databaseConfig),
+		EventsModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
---
+++ src/events/events.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { PaymentQueueService } from './payment-queue/payment-queue.service'
+import { RabbitmqService } from './rabbitmq/rabbitmq.service'
+
+@Module({
+	imports: [ConfigModule],
+	providers: [RabbitmqService, PaymentQueueService]
+})
+export class EventsModule {}
