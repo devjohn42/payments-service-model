61270eda154a79268d7106062b1a2b88ebdcbdc3
devjohn42 <devjohncb@gmail.com>
2026-08-30 18:56:16 -0300

    :sparkles: feat: implements the DLQ service and controller

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { DlqController } from './dlq/dlq.controller'
+import { DlqService } from './dlq/dlq.service'
+import { PaymentConsumerService } from './payment-consumer/payment-consumer.service'
 import { PaymentQueueService } from './payment-queue/payment-queue.service'
 import { RabbitmqService } from './rabbitmq/rabbitmq.service'
-import { PaymentConsumerService } from './payment-consumer/payment-consumer.service';
 
 @Module({
 	imports: [ConfigModule],
-	providers: [RabbitmqService, PaymentQueueService, PaymentConsumerService]
+	controllers: [DlqController],
+	providers: [RabbitmqService, PaymentQueueService, PaymentConsumerService, DlqService]
 })
 export class EventsModule {}
