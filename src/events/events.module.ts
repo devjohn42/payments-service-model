3dbd6f357c38d2873182d9cdd7f9910c065fd32f
devjohn42 <devjohncb@gmail.com>
2026-09-03 22:25:04 -0300

    :sparkles: feat: implements metrics controller

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { DlqController } from './dlq/dlq.controller'
 import { DlqService } from './dlq/dlq.service'
+import { MetricsController } from './metrics/metrics.controller'
 import { PaymentConsumerService } from './payment-consumer/payment-consumer.service'
 import { PaymentQueueService } from './payment-queue/payment-queue.service'
 import { RabbitmqService } from './rabbitmq/rabbitmq.service'
 
 @Module({
 	imports: [ConfigModule],
-	controllers: [DlqController],
+	controllers: [DlqController, MetricsController],
 	providers: [RabbitmqService, PaymentQueueService, PaymentConsumerService, DlqService]
 })
 export class EventsModule {}
